ef239ea1b97ac91a17880952b35930fdce6caa15
qrzbrz@yahoo.de <qrzbrz@yahoo.de>
2021-08-04 17:22:39 +0200

    fixes

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import params from "./params"
 try {
     params.verify()
     const fileName = addSshKey(params.name, params.key)
+    core.setOutput("key-file", fileName)
     core.info(`file ${fileName} created`)
 } catch (error) {
     core.setFailed(error.message)
